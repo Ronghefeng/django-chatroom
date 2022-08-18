35a980a1156c33f29915fb87ead604e5fdda46a9
rhf <2427219623@qq.com>
2022-08-18 16:25:40 +0800

    添加uwsgi

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # django-chatroom
+
+### gunicorn 托管项目支持 websocket
+gunicorn django_chatroom.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
+
+### uWSGI 托管项目支持 websocket
+启动：uwsgi --ini ./uwsgi.ini --http-websockets
+停止：uwsgi --stop ./uwsgi.ini
--- django_chatroom/settings.py
+++ django_chatroom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-nq#d)_jm6&3w+)qtmx=rx67-wmc=-ozwtas)h(425ufmyx283$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
